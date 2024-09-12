46565250fa4e0bd0c18ba33de8e62974912c8f9b
Yash Bhappa <ybhappa@gmail.com>
2024-09-12 18:45:25 +0530

    final Deployment in npm

---
+++ packages/cli/notebook.js
@@ -0,0 +1,1 @@
+[{"content":"","type":"code","id":"3u8z4"},{"content":"","type":"text","id":"hff4t"}]
--- packages/local-api/src/index.ts
+++ packages/local-api/src/index.ts
@@ -1,34 +1,32 @@
-import express from 'express';
-import { createProxyMiddleware } from 'http-proxy-middleware';
-import path from 'path';
-import { createCellsRouter } from './routes/cells';
+import express from "express";
+import { createProxyMiddleware } from "http-proxy-middleware";
+import path from "path";
+import { createCellsRouter } from "./routes/cells";
 
 export const serve = (
-  port: number,
-  filename: string,
-  dir: string,
-  useProxy: boolean
+	port: number,
+	filename: string,
+	dir: string,
+	useProxy: boolean
 ) => {
-  const app = express();
+	const app = express();
 
-  app.use(createCellsRouter(filename, dir));
+	app.use(createCellsRouter(filename, dir));
 
-  if (useProxy) {
-    app.use(
-      createProxyMiddleware({
-        target: 'http://localhost:3000',
-        ws: true,
-        logLevel: 'silent',
-      })
-    );
-  } else {
-    const packagePath = require.resolve(
-      '@jsnote/local-client/build/index.html'
-    );
-    app.use(express.static(path.dirname(packagePath)));
-  }
+	if (useProxy) {
+		app.use(
+			createProxyMiddleware({
+				target: "http://localhost:3000",
+				ws: true,
+				logLevel: "silent",
+			})
+		);
+	} else {
+		const packagePath = require.resolve("@ybook/local-client/build/index.html");
+		app.use(express.static(path.dirname(packagePath)));
+	}
 
-  return new Promise<void>((resolve, reject) => {
-    app.listen(port, resolve).on('error', reject);
-  });
+	return new Promise<void>((resolve, reject) => {
+		app.listen(port, resolve).on("error", reject);
+	});
 };
